41f1a8c46c6e821d142d805a21cc486cfd784d52
figtereiscoding <quererqabv@gmail.com>
2023-04-21 16:35:01 +0300

    conditionalstatements-lab

---
+++ A2ConditionalStatements/Lab/P05Number100to200.java
@@ -0,0 +1,18 @@
+package A2ConditionalStatements.Lab;
+
+import java.util.Scanner;
+
+public class P05Number100to200 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+
+        if (num <= 100) {
+            System.out.println("Less than 100");
+        } else if (num >= 200) {
+            System.out.println("Greater than 200");
+        } else {
+            System.out.println("Between 100 and 200");
+        }
+    }
+}

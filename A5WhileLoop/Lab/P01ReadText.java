a4a30cccdcadf82b5aeab16877077ea7a65173e5
figtereiscoding <quererqabv@gmail.com>
2023-05-12 14:57:56 +0300

    Create P01ReadText.java

---
+++ A5WhileLoop/Lab/P01ReadText.java
@@ -0,0 +1,18 @@
+package A5WhileLoop.Lab;
+
+import java.util.Scanner;
+
+public class P01ReadText {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("Stop")) {
+
+            System.out.println(input);
+
+            input = scanner.nextLine();
+        }
+    }
+}

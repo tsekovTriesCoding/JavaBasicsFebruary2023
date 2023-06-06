373131f2bbad7de469500ef3772e1ab60349c3b5
figtereiscoding <quererqabv@gmail.com>
2023-06-06 18:12:29 +0300

    Create P04NumbersDividedBy3WithoutReminder.java

---
+++ A5WhileLoop/MoreExercises/P04NumbersDividedBy3WithoutReminder.java
@@ -0,0 +1,15 @@
+package A5WhileLoop.MoreExercises;
+
+import java.util.Scanner;
+
+public class P04NumbersDividedBy3WithoutReminder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 3; i <= 99 ; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}

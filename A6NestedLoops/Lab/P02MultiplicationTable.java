25924b16069b61ffa6ae63e6b67f61d3e455e91c
figtereiscoding <quererqabv@gmail.com>
2023-06-12 17:55:36 +0300

    Create P02MultiplicationTable.java

---
+++ A6NestedLoops/Lab/P02MultiplicationTable.java
@@ -0,0 +1,16 @@
+package A6NestedLoops.Lab;
+
+import java.util.Scanner;
+
+public class P02MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                int result = i * j;
+                System.out.printf("%d * %d = %d%n", i, j, result);
+            }
+        }
+    }
+}

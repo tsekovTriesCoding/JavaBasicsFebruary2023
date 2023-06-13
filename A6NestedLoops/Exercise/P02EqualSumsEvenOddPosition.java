d788a3b0cfc88db16158e1363c05fe299a0ceb76
figtereiscoding <quererqabv@gmail.com>
2023-06-13 17:22:59 +0300

    Create P02EqualSumsEvenOddPosition.java

---
+++ A6NestedLoops/Exercise/P02EqualSumsEvenOddPosition.java
@@ -0,0 +1,35 @@
+package A6NestedLoops.Exercises;
+
+import java.util.Scanner;
+
+public class P02EqualSumsEvenOddPosition {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
+
+        for (int i = num1; i <= num2; i++) {
+
+            int currentNum = i;
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int j = 6; j >= 1; j--) {
+                int digit = currentNum % 10;
+
+                if (j % 2 == 0) {
+                    evenSum += digit;
+                } else {
+                    oddSum += digit;
+                }
+
+                currentNum = currentNum / 10;
+            }
+            if (evenSum == oddSum) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}

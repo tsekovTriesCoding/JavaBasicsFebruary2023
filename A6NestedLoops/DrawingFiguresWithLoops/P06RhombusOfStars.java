632d721163e76b492342eb9ec18cdb016070c636
figtereiscoding <quererqabv@gmail.com>
2023-06-23 19:17:10 +0300

    Create P06RhombusOfStars.java

---
+++ A6NestedLoops/DrawingFiguresWithLoops/P06RhombusOfStars.java
@@ -0,0 +1,33 @@
+package A6NestedLoops.DrawingFiguresWithLoops;
+
+import java.util.Scanner;
+
+public class P06RhombusOfStars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for (int j = 1; j < i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for (int j = 1; j < i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}

03ba6f28a4ac63dba47c9ee26277c2a81772be55
figtereiscoding <quererqabv@gmail.com>
2023-06-23 19:16:57 +0300

    Create P05SquareFrame.java

---
+++ A6NestedLoops/DrawingFiguresWithLoops/P05SquareFrame.java
@@ -0,0 +1,30 @@
+package A6NestedLoops.DrawingFiguresWithLoops;
+
+import java.util.Scanner;
+
+public class P05SquareFrame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("+");
+        for (int j = 0; j < n - 2; j++) {
+            System.out.print(" -");
+        }
+        System.out.println(" +");
+
+        for (int i = 0; i < n - 2; i++) {
+            System.out.print("|");
+            for (int j = 0; j < n - 2; j++) {
+                System.out.print(" -");
+            }
+            System.out.println(" |");
+        }
+        System.out.print("+");
+        for (int i = 0; i < n - 2; i++) {
+            System.out.print(" -");
+        }
+        System.out.print(" +");
+    }
+}
